4a6998ac3081ef2412eec38acaf34bf5096f8f72
ZIANI Abderrahman <ziani.abderrahman@gmail.com>
2015-05-21 19:23:32 +0200

    modification

---
+++ CoursJava2015/src/be/cardinalmercier/tp14/exercice2/Etudiant.java
@@ -0,0 +1,21 @@
+package be.cardinalmercier.tp14.exercice2;
+
+/**
+ * @author Abderrahman
+ *
+ */
+public class Etudiant {
+	
+	private String nom;
+	private String prenom;
+	private int numero;
+	
+	
+	 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
