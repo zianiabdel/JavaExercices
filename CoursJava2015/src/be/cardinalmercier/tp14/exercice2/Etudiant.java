74ec423daee31114e79eca09284c5a6349e713fd
ZIANI Abderrahman <ziani.abderrahman@gmail.com>
2015-05-21 19:35:10 +0200

    ok

--- CoursJava2015/src/be/cardinalmercier/tp14/exercice2/Etudiant.java
+++ CoursJava2015/src/be/cardinalmercier/tp14/exercice2/Etudiant.java
@@ -1,21 +1,51 @@
 package be.cardinalmercier.tp14.exercice2;
 
-/**
- * @author Abderrahman
- *
- */
+
+import java.util.ArrayList;
+
+
 public class Etudiant {
-	
 	private String nom;
-	private String prenom;
-	private int numero;
-	
-	
-	 
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static int dernierNumAttribue = 0;
+	private int num;
+	private ArrayList<Integer> listePoints = new ArrayList<Integer>();
+			public static ____________________ listeEtu = new __________________________
+			public Etudiant(String nom){
+		this.nom = nom;
+		this.num = ++ dernierNumAttribue; //pré-incrémentation
 	}
-
+	public double moyenne(){
+		double somme = 0;
+		for (Integer points : ___________________) {
+			somme += points;
+		}
+		if (listePoints.size() == 0) return 0;
+		return somme / listePoints.size();
+	}
+	public String toString() {
+		StringBuffer s = new StringBuffer("Nom : " + nom + " num : "+num+"\nPoints : ");
+		for (Integer points : ____________________) {
+			s.append("\t" + points);
+		}
+		s.append("\n" + "moyenne = " + this.moyenne()+"\n");
+		String s2 = new String(s);
+		return s2;
+	}
+	public static void ajouteEtu(Etudiant newEtu){
+		____________.______(newEtu);
+	}
+	public void ajoutePoint(int newPoint){
+		____________.______(newPoint);
+	}
+	/**
+	 * @return une chaine de caractères qui représentent tous les étudiants de la liste
+	 */
+	public static String toStringAll(){
+		StringBuffer sb = new StringBuffer();
+		for (Etudiant etudiant : __________________) {
+			sb.append(etudiant.toString());
+		}
+		String s = new String(sb);
+		return s;
+	}
 }
