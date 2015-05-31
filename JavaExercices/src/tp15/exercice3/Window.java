e7fe2a3d916b148290196ce56383eff003dbb8fa
Abderrahman <Abderrahman@ZEDONE>
2015-05-31 23:45:07 +0200

    ok

--- JavaExercices/src/tp15/exercice3/Window.java
+++ JavaExercices/src/tp15/exercice3/Window.java
@@ -1,12 +1,14 @@
 package tp15.exercice3;
 
+import java.awt.event.*;
+
 import javax.swing.JFrame;
 
 /**
  * @author Abderrahman
  *
  */
-public class Window {
+public class Window implements  {
   public static void main(String[] args){       
     JFrame fenetre = new JFrame();
    
@@ -16,6 +18,10 @@ public class Window {
     fenetre.setVisible(true);
     System.out.println("The Window is shown!");
     fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-  }       
+  }  
+  
+  public void mouseClicked(MouseEvent event){
+	  System.out.println ("clic dans fenetre") ;
+  }
   
 }
