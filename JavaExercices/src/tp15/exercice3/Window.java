208b11cbb4b2562b03b622842c70a73150ef860c
Abderrahman <Abderrahman@ZEDONE>
2015-06-01 00:30:40 +0200

    ok

--- JavaExercices/src/tp15/exercice3/Window.java
+++ JavaExercices/src/tp15/exercice3/Window.java
@@ -3,25 +3,35 @@ package tp15.exercice3;
 import java.awt.event.*;
 
 import javax.swing.JFrame;
-
 /**
  * @author Abderrahman
  *
  */
-public class Window implements  {
-  public static void main(String[] args){       
-    JFrame fenetre = new JFrame();
-   
-    System.out.println("The window will be shown");
-    fenetre.setBounds(750,300, 400,200);
-    fenetre.setTitle("My First Window");
-    fenetre.setVisible(true);
-    System.out.println("The Window is shown!");
-    fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-  }  
-  
-  public void mouseClicked(MouseEvent event){
-	  System.out.println ("clic dans fenetre") ;
-  }
-  
+@SuppressWarnings("serial")
+public class Window extends JFrame implements ActionListener, MouseListener {
+	public static void main(String[] args){       
+		JFrame fenetre = new JFrame();
+
+		System.out.println("The window will be shown");
+		fenetre.setBounds(750,300, 400,200);
+		fenetre.setTitle("My First Window");
+		fenetre.setVisible(true);
+		System.out.println("The Window is shown!");
+		fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+	
+	@SuppressWarnings({"unused" })
+	public void mouseClicked (MouseEvent ev) {
+		System.out.println ("clic dans fenetre") ;
+	}
+	public void mouseEntered(MouseEvent arg0) {	
+	}
+	public void mouseExited(MouseEvent arg0) {
+	}
+	public void mousePressed(MouseEvent arg0) {
+	}
+	public void mouseReleased(MouseEvent arg0) {
+	}
+	public void actionPerformed(ActionEvent arg0) {
+	}
 }
